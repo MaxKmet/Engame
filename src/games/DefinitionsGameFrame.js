cf1f2a3b9cd324ee711b81dd301aacfe382d472a
Max Kmet <maxkmet@gmail.com>
2020-05-25 11:45:08 +0300

    DefinitionGame finished

--- src/games/DefinitionsGameFrame.js
+++ src/games/DefinitionsGameFrame.js
@@ -7,41 +7,64 @@ import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 
 
-export default class DefinitionsGameFrame extends React.Component  {
+export default class DefinitionsGameFrame extends React.Component {
 
-  constructor({classes}) {
+  constructor({ classes }) {
     super();
 
     this.state = {
       numOptions: 4,
-      definition: "A long definition of a difficult term in English",
-      words: [{word:"Word1",correct:false, color:"primary"},{word:"Word2",correct:true, color:"primary"}, 
-      {word:"Word3",correct:false, color:"primary"}, {word:"Word4",correct:false, color:"primary"}],      
+      definition: "Please press NEXT to start a game",
+      words: [{ word: "Word1", correct: false, color: "primary" }, { word: "Word2", correct: true, color: "primary" },
+      { word: "Word3", correct: false, color: "primary" }, { word: "Word4", correct: false, color: "primary" }],
       classes: classes,
       points: 10,
     };
-    
+
   }
 
-  checkSelection(e, predictedWord){
+  checkSelection(e, predictedWord) {
     e.preventDefault();
     let addPoints = 0;
     let correctWord = this.state.words.find((word) => word.correct).word;
-    if(predictedWord == correctWord){
-      addPoints =10;
+    if (predictedWord == correctWord) {
+      addPoints = 10;
     }
 
     this.setState((state) => ({
-      words: state.words.map((word) => ({word: word.word, correct: word.correct, color: word.correct ? "secondary": "primary"})),
+      words: state.words.map((word) => ({ word: word.word, correct: word.correct, color: word.correct ? "secondary" : "primary" })),
       points: state.points + addPoints
     }))
 
   }
 
-  updateEverything(e){
-    e.preventDefault();
-    let names = ['iliakan', 'remy', 'jeresig', "hello"];
+  getRequestsForDefinition(words){
+    words = words.map( word => word.replace(" ","_"));
+    let requests_definition = words.map( name => fetch("https://wordsapiv1.p.rapidapi.com/words/"+ name +"/definitions", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+        "x-rapidapi-key": "e35230f3b8msh39f53e53d676724p1619c3jsn99097ecfe42a"
+      }
+    }));
+    return requests_definition;
+  }
 
+
+  updateEverything(e) {
+    e.preventDefault();
+
+    var correct_word_def = ""
+    var correctWord = ""
+    var words_definitions = []
+    this.setState((state) => ({
+      definition: ""      
+    }))
+
+    var updatedSuccessfully = false;
+
+    
+    let names = ['iliakan', 'remy', 'jeresig', "hello"];
     let requests = names.map(_ => fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
       "method": "GET",
       "headers": {
@@ -50,21 +73,77 @@ export default class DefinitionsGameFrame extends React.Component  {
       }
     }));
 
-     Promise.all(requests)
-       .then(responses => {        
-     
-         return responses;
-       })
+    var random_words = [];
+
+    Promise.all(requests)
+      .then(responses => {
+        return responses;
+      })
+      .then(responses => Promise.all(responses.map(r => r.json())))
+      .then(results => {
+        console.log("BASIC RESULTS")
+        
+        random_words = results.map((res) => (res.word));
+        random_words = random_words.filter(word => (word != undefined) && (word.length > 6));
+        console.log(random_words)
+        //REMOVE
+        
+      }).then(()=>{
+      
+    
+    Promise.all(this.getRequestsForDefinition(random_words))
+      .then(responses => {
+        return responses;
+      })
+      .then(responses => Promise.all(responses.map(r => r.json())))
+      .then(results => {
+       results = results.filter(res => !("success" in res));
+       words_definitions = results.map((res) => ({ "word": res.word, "definition": res.definitions }));
+       words_definitions =  words_definitions.filter(word => word != undefined);
+       console.log("DEFS RESPONSES: ")
+       console.log(results)
        
-       .then(responses => Promise.all(responses.map(r => r.json())))
-       .then(results => {this.setState((state) => ({
-        words: results.map((res) => ({word: res.word, correct: false, color:"primary"})),
-      }))});
+       var word;
+       console.log("WORD_DEFS")
+       console.log(words_definitions)
+       for(word of words_definitions){
+         console.log(word);
 
+         console.log(word)
+         if ((word["definition"]) && (word["definition"].length > 0)){           
+           correctWord = word["word"]
+           correct_word_def = word["definition"][0]["definition"]
+           
+         }
+
+
+       }
+      
+           
+
+      }).then(()=>{
+        if(correct_word_def){    
+        this.setState((state) => ({
+          definition: correct_word_def,
+          words: words_definitions.map((res) => ({key: res.word + correctWord,  word: res.word, correct: res.word == correctWord, color: "primary" })),
+        }))
+        
       }
+      else{
+        this.setState((state) => ({
+          definition: "Bad API response, press NEXT again",          
+        }))
+      }
+      })
+    })
+  
 
-  resetWords(){
-    for(let i = 0; i < this.state.numOptions; i++){
+
+  
+}
+
+  resetWords() {
+    for (let i = 0; i < this.state.numOptions; i++) {
       fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
         "method": "GET",
         "headers": {
@@ -72,105 +151,103 @@ export default class DefinitionsGameFrame extends React.Component  {
           "x-rapidapi-key": "e35230f3b8msh39f53e53d676724p1619c3jsn99097ecfe42a"
         }
       })
-      .then(res => res.json())
-            .then(
-              (result) => {          
-                console.log(result);
-                this.setState((state) => ({
-                  words: state.words.concat([{word:result.word,correct:false, color:"primary"}]),
-                }))
-              },        
-              (error) => {
-                this.setState({
-                  definition: error,
-                  words: []            
-                });
-              }
-            )
-      }
-
-
+        .then(res => res.json())
+        .then(
+          (result) => {
+            console.log(result);
+            this.setState((state) => ({
+              words: state.words.concat([{ word: result.word, correct: false, color: "primary" }]),
+            }))
+          },
+          (error) => {
+            this.setState({
+              definition: error,
+              words: []
+            });
+          }
+        )
+    }
   }
 
-  getUpdatedWords(){
+  getUpdatedWords() {
     return Promise.all([this.resetWords()])
   }
 
-  resetField(e){
+  resetField(e) {
     e.preventDefault();
-    
+
     this.setState((state) => ({
       words: [],
     }))
-    
-    
-    this.getUpdatedWords().then(()=>{   
 
-    let correctWordInd = Math.floor(Math.random() * this.state.numOptions); 
-    let correctWord = this.state.words[0].word;
-    
-    fetch("https://wordsapiv1.p.rapidapi.com/words/" + correctWord + "/definitions", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-        "x-rapidapi-key": "e35230f3b8msh39f53e53d676724p1619c3jsn99097ecfe42a"
-      }
+
+    this.getUpdatedWords().then(() => {
+
+      let correctWordInd = Math.floor(Math.random() * this.state.numOptions);
+      let correctWord = this.state.words[0].word;
+
+      fetch("https://wordsapiv1.p.rapidapi.com/words/" + correctWord + "/definitions", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+          "x-rapidapi-key": "e35230f3b8msh39f53e53d676724p1619c3jsn99097ecfe42a"
+        }
+      })
+        .then(res => res.json())
+        .then(
+          (result) => {
+            console.log(result);
+            this.setState((state) => ({
+              words: state.words.map((word) => ({ word: word.word, correct: correctWord == word.word, color: word.color })),
+              definition: result["definitions"]["definition"]
+
+            }))
+          },
+          (error) => {
+            this.setState({
+              definition: error,
+              words: []
+            });
+          }
+        )
     })
-    .then(res => res.json())
-          .then(
-            (result) => {          
-              console.log(result);
-              this.setState((state) => ({
-                words: state.words.map((word) => ({word: word.word, correct: correctWord == word.word, color: word.color})),
-                definition: result["definitions"]["definition"]
-
-              }))
-            },        
-            (error) => {
-              this.setState({
-                definition: error,
-                words: []            
-              });
-            }
-          )
-        })
 
 
 
-    }
-  
-  render(){
-  return (
-    <div className={this.state.classes.root}>
-      <div className={this.state.classes.section1}>
-        <Grid container alignItems="center">
-          <Grid item xs>
-            <Typography gutterBottom variant="h4">
-              What is word for this definition ?
+  }
+
+  render() {
+    return (
+      <div className={this.state.classes.root}>
+        <div className={this.state.classes.section1}>
+          <Grid container alignItems="center">
+            <Grid item xs>
+              <Typography gutterBottom variant="h4">
+                What is word for this definition ?
             </Typography>
+            </Grid>
+            <Grid item>
+            </Grid>
           </Grid>
-          <Grid item>
-          </Grid>
-        </Grid>
-        <Typography gutterBottom variant="body1">
-              {this.state.definition}
-        </Typography>
-      </div>
-      <Divider variant="middle"/>
-      <div className={this.state.classes.section2}>
-        <div>
-        {this.state.words.map((wordItem) => (
-          <Chip onClick ={(e) => this.checkSelection(e, wordItem.word)} key={wordItem.word} color={wordItem.color} className={this.state.classes.chip} label= {wordItem.word} />          
-        ))} 
-        <Chip onClick ={(e) => this.updateEverything(e)} color="primary" className={this.state.classes.chip} label= "NEXT" />
+          <Typography gutterBottom variant="body1">
+            {this.state.definition}
+          </Typography>
+        </div>
+        <Divider variant="middle" />
+        <div className={this.state.classes.section2}>
+          <div>
+            {this.state.words.map((wordItem) => (
+              <Chip onClick={(e) => this.checkSelection(e, wordItem.word)} key={wordItem.word} color={wordItem.color} className={this.state.classes.chip} label={wordItem.word} />
+            ))}
+            <Chip onClick={(e) => this.updateEverything(e)} color="primary" className={this.state.classes.chip} label="NEXT" />
 
+          </div>
+        </div>
+        <div className={this.state.classes.section3}>
+          <Button color="primary">Score: {this.state.points}</Button>
         </div>
       </div>
-      <div className={this.state.classes.section3}>
-        <Button color="primary">Score: {this.state.points}</Button>
-      </div>
-    </div>
-  );
+    );
   }
 }
 
